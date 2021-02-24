b2817aee0d983edd1874330233682b71411c034e
hitoshi-suito <htshts153@gmail.com>
2021-02-24 16:17:55 +0900

    一覧に投稿者名が表示されるようにした

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_075024) do
+ActiveRecord::Schema.define(version: 2021_02_24_062214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2021_02_23_075024) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_pictures_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_02_23_075024) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "pictures", "users"
 end
