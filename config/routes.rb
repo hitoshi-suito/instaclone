1fd4d23dff3c8d5ffe743506ee95faa576c69203
hitoshi-suito <htshts153@gmail.com>
2021-02-27 19:30:41 +0900

    fix: root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "pictures#index"
   resources :favorites, only: [:create, :destroy, :index]
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
